1da0e3d33b1a9b2083868cc4ca76c6b5c8b7f83f
Maureen Riley <mriley@adaptigroup.com>
2017-11-08 12:38:44 -0600

    Thank God for source control

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GridComponent } from './grid/grid.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GridComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule
